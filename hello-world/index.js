e6b4119b0b144b70cb87e71861316c2ea7622873
Gurmu Gessese <gurmugt@gmail.com>
2023-02-23 00:25:49 +0530

    some edition!

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,5 +1,38 @@
-const hello=()=>{
-    console.log("Hello GitHub!");
-};
+const fs = require("fs");
+fs.writeFile(
+    "sample.txt",
+    "Hello World. Welcome to Node.js File System module.",
+    (err) => {
+      if (err) throw err;
+      console.log("File created!");
+    }
+  );
 
-hello();
+//to read file
+  fs.readFile("sample.txt", (err, data) => {
+    if (err) throw err;
+    console.log(data.toString());
+  });
+
+//We can use appendFile() to add more content to the file already created.
+  fs.appendFile("sample.txt", " This is my updated content", (err) => {
+    if (err) throw err;
+    console.log("File updated!");
+  });
+
+  //We can use rename() to rename an existing file.
+  fs.rename("sample.txt", "test.txt", (err) => {
+    if (err) throw err;
+    console.log("File name updated!");
+  });
+
+
+  //We can use the unlink() to delete the files in the file system.
+//   fs.unlink("test.txt", (err) => {
+//     if (err) throw err;
+//     console.log("File test.txt deleted successfully!");
+//   });
+
+
+//to import a package
+//import {ESlint} from "eslint";
