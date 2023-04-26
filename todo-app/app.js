10fe6a3488f6be315b2aaac79f0b3897b090294c
Gurmu Gessese <gurmugt@gmail.com>
2023-04-26 21:23:18 +0530

    Final commit

--- todo-app/app.js
+++ todo-app/app.js
@@ -25,7 +25,7 @@ app.use(flash());
 
 app.use(
   session({
-    secret: "my-super-secret-key-21728172615261562",
+    secret: "my-super-secret-key-123456789876123456788",
     cookies: {
       maxAge: 24 * 60 * 60 * 1000,
     },
